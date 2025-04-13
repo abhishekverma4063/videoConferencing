bb1666f7fdcec9873095d693b9755248ae0da748
@2690806kundansingh <165538337+2690806kundansingh@users.noreply.github.com>
2025-04-13 23:19:12 +0530

    Initial commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
+    const connectionDb = await mongoose.connect("mongodb+srv://kdsc:9336085628@cluster0.xo8xfay.mongodb.net/")
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
